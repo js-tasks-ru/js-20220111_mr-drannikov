c7f730fc55cf5ef33fe6c7afd73ab481258669e0
Vladimir Drannikov <v.drannikov@alef.im>
2022-01-20 10:09:05 +0600

    Added all tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const arrKeys = path.split('.');
+  let count = 0;
 
+  const getter = (obj) => {
+    if (typeof obj !== 'object') {
+      return obj;
+    }
+    const key = arrKeys[count];
+    const newObj = obj[key];
+    count += 1;
+    return getter(newObj);
+  };
+
+  return getter;
 }
